125da269003558d6fe1b84d4b7b7f8c01acc6249
kgco <43102785+kgco@users.noreply.github.com>
2020-08-27 07:58:37 +0800

    Add files via upload

---
+++ src/html_render.py
@@ -0,0 +1,5 @@
+with open('../data/comments_data.json', 'r',encoding='utf8') as f:
+    data = f.read()
+jsfile = 'var data = ' + data
+with open('../html/data.js', 'w', encoding='utf8') as f:
+    f.write(jsfile)
